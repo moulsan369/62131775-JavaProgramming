3cc80b4282c3ab825c68c754c20772033ba99af0
Moul San <san.m.106bcl@gmail.com>
2024-01-25 14:20:05 +0700

    Sửa lại file lỗi thư mục

---
+++ Lab1/Lab1Bai2.java
@@ -0,0 +1,18 @@
+/**
+ * 
+ */
+
+/**
+ * 
+ */
+public class Lab1Bai2 {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ src/helloJava.java
@@ -0,0 +1,18 @@
+/**
+ * 
+ */
+
+/**
+ * 
+ */
+public class helloJava {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.print("Hello Moul Sàn");
+	}
+
+}
