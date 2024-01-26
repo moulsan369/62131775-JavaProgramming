83680bf03338693259ee42666e147ddf4aae6c3e
Moul San <san.m.106bcl@gmail.com>
2024-01-26 13:57:27 +0700

    Up test package

---
+++ Lab3/moulsan369/Lab3bai2.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+package moulsan369;
+
+/**
+ * 
+ */
+public class Lab3bai2 {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
