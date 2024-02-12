5a855ee6b87c5c13fdb44461f7250e9d6e118600
Moul Sàn <94727379+moulsan369@users.noreply.github.com>
2024-02-12 14:12:49 +0700

    Update Lab3bai1.java
    Up Lab 3 bài 1

--- Lab3/moulsan369/Lab3bai1.java
+++ Lab3/moulsan369/Lab3bai1.java
@@ -2,7 +2,7 @@
  * 
  */
 package moulsan369;
-
+import java.util.Scanner;
 /**
  * 
  */
@@ -13,7 +13,27 @@ public class Lab3bai1 {
 	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		Scanner scanner = new Scanner(System.in);
+		
+		System.out.print("Nhập một số nguyên bất kỳ: ");
+		int soBatKy = scanner.nextInt();
+		boolean oK = true;
+		
+		for (int i = 2; i <= soBatKy-1; i++) {
+			if(soBatKy % i == 0) {
+				oK = false;
+				break;
+			}
+		}
+		
+		if (oK) {
+			System.out.println(soBatKy + " Là một số nguyên tố");
+		} else {
+			System.out.println(soBatKy + " Không phải là số nguyên tố");
+		}
+		
+		scanner.close();
+		
 	}
 
 }
