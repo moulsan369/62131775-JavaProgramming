1941b1d6f53af529b386d2e059c1feeb227ca8c6
Moul Sàn <94727379+moulsan369@users.noreply.github.com>
2024-02-13 16:04:41 +0700

    Update Lab3bai2.java
    Up lab 3 bài 2

--- Lab3/moulsan369/Lab3bai2.java
+++ Lab3/moulsan369/Lab3bai2.java
@@ -13,7 +13,17 @@ public class Lab3bai2 {
 	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		/*
+		 * int x = 8; for (int i = 1; i <=10; i++) {
+		 * System.out.printf("%d x %d = %d",x,i, x*i); System.out.println(); }
+		 */
+		for(int i = 1; i<=9; i++)
+			for (int j = 1; j<= 10; j++) {
+				System.out.printf("%d x %d = %d", i,j,i*j);
+				System.out.println();
+			}
+		
+		
 	}
 
 }
