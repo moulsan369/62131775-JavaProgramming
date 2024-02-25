94a3ec0c1aef3b5a80e6aa5acab8700355e1ff3a
Moul Sàn <94727379+moulsan369@users.noreply.github.com>
2024-02-25 23:06:42 +0700

    Update Lab4Bai1.java
    Up lab 4

--- Lab4/moulsan369/Lab4Bai1.java
+++ Lab4/moulsan369/Lab4Bai1.java
@@ -1,19 +1,34 @@
-/**
- * 
- */
 package moulsan369;
+import java.util.Scanner;
 
-/**
- * 
- */
 public class Lab4Bai1 {
 
-	/**
-	 * @param args
-	 */
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+    public static void main(String[] args) {
+        SanPham sp = new SanPham("áo love", 199000, 1000);
+        sp.xuat();
+    }
 
-	}
+}
 
+class SanPham {
+    private String tenSp;
+    private double donGia;
+    private double giamGia;
+
+    public SanPham(String tenSp, double donGia, double giamGia) {
+        this.tenSp = tenSp;
+        this.donGia = donGia;
+        this.giamGia = giamGia;
+    }
+
+    public double getThueNhapKhau() {
+        return 0.1 * donGia;
+    }
+
+    public void xuat() {
+        System.out.println("Tên sản phẩm: " + tenSp);
+        System.out.println("Đơn giá: " + donGia);
+        System.out.println("Giảm giá: " + giamGia);
+        System.out.println("Thuế nhập khẩu: " + getThueNhapKhau());
+    }
 }
