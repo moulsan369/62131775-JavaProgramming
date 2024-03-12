9933d28b72645bace1a4ab3ae4519072aa933ba2
Moul Sàn <94727379+moulsan369@users.noreply.github.com>
2024-03-13 00:18:06 +0700

    Up bai 1 lab 6

--- Lab6/moulsan369/Lab6Bai1.java
+++ Lab6/moulsan369/Lab6Bai1.java
@@ -1,5 +1,33 @@
 package moulsan369;
 
-public class Lab6Bai1 {
+import java.util.Scanner;
 
+public class Lab6Bai1 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Nhập họ và tên: ");
+        String fullName = scanner.nextLine().trim();
+
+        // Tìm vị trí của dấu cách đầu tiên và dấu cách tiếp theo sau dấu cách đầu tiên
+        int firstSpaceIndex = fullName.indexOf(' ');
+        int nextSpaceIndex = fullName.indexOf(' ', firstSpaceIndex + 1);
+
+        // Tách họ, tên đệm và tên
+        String lastName = fullName.substring(0, firstSpaceIndex).toUpperCase();
+        String middleName = "";
+        String firstName = "";
+
+        // Kiểm tra xem có tên đệm không
+        if (nextSpaceIndex != -1) {
+            middleName = fullName.substring(firstSpaceIndex + 1, nextSpaceIndex).toLowerCase();
+            firstName = fullName.substring(nextSpaceIndex + 1).toUpperCase();
+        } else {
+            firstName = fullName.substring(firstSpaceIndex + 1).toUpperCase();
+        }
+
+        // In ra kết quả
+        System.out.println("Họ: " + lastName);
+        System.out.println("Tên đệm: " + middleName);
+        System.out.println("Tên: " + firstName);
+    }
 }
---
+++ Lab6/moulsan369/Lab6bai2.java
@@ -0,0 +1,10 @@
+package moulsan369;
+
+public class Lab6bai2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
