2a32fdb25e4404583527624da23e9dfe500dc0d0
Moul Sàn <94727379+moulsan369@users.noreply.github.com>
2024-03-13 00:36:42 +0700

    Up bai 2 lab 6

--- Lab6/moulsan369/Lab6Bai1.java
+++ Lab6/moulsan369/Lab6Bai1.java
@@ -8,16 +8,16 @@ public class Lab6Bai1 {
         System.out.print("Nhập họ và tên: ");
         String fullName = scanner.nextLine().trim();
 
-        // Tìm vị trí của dấu cách đầu tiên và dấu cách tiếp theo sau dấu cách đầu tiên
+       
         int firstSpaceIndex = fullName.indexOf(' ');
         int nextSpaceIndex = fullName.indexOf(' ', firstSpaceIndex + 1);
 
-        // Tách họ, tên đệm và tên
+        
         String lastName = fullName.substring(0, firstSpaceIndex).toUpperCase();
         String middleName = "";
         String firstName = "";
 
-        // Kiểm tra xem có tên đệm không
+       
         if (nextSpaceIndex != -1) {
             middleName = fullName.substring(firstSpaceIndex + 1, nextSpaceIndex).toLowerCase();
             firstName = fullName.substring(nextSpaceIndex + 1).toUpperCase();
@@ -25,7 +25,7 @@ public class Lab6Bai1 {
             firstName = fullName.substring(firstSpaceIndex + 1).toUpperCase();
         }
 
-        // In ra kết quả
+     
         System.out.println("Họ: " + lastName);
         System.out.println("Tên đệm: " + middleName);
         System.out.println("Tên: " + firstName);
--- Lab6/moulsan369/Lab6bai2.java
+++ Lab6/moulsan369/Lab6bai2.java
@@ -1,10 +1,59 @@
 package moulsan369;
 
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class Lab6bai2 {
+    static class SanPham_bai2l6 {
+        private String tenSp;
+        private double donGia;
+        private String hang;
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+        public SanPham_bai2l6() {}
+        SanPham_bai2l6(String tenSp, double donGia, String hang) {
+            this.tenSp = tenSp;
+            this.donGia = donGia;
+            this.hang = hang;
+        }
 
-	}
+        public void nhap() {
+            Scanner scanner = new Scanner(System.in);
+            System.out.print("Nhập tên sản phẩm: ");
+            this.tenSp = scanner.nextLine();
+            System.out.print("Nhập giá sản phẩm: ");
+            this.donGia = scanner.nextDouble();
+            scanner.nextLine(); 
+            System.out.print("Nhập hãng sản phẩm: ");
+            this.hang = scanner.nextLine();
+        }
 
+        public void xuat() {
+            System.out.println("Tên sản phẩm: " + this.tenSp);
+            System.out.println("Giá sản phẩm: " + this.donGia);
+            System.out.println("Hãng sản phẩm: " + this.hang);
+        }
+
+        public String getHang() {
+            return this.hang;
+        }
+    }
+
+    public static void main(String[] args) {
+        ArrayList<SanPham_bai2l6> danhSachSanPham = new ArrayList<>();
+
+        Scanner scanner = new Scanner(System.in);
+        for (int i = 0; i < 5; i++) {
+            System.out.println("Nhập thông tin sản phẩm thứ " + (i + 1));
+            SanPham_bai2l6 sanPham = new SanPham_bai2l6();
+            sanPham.nhap();
+            danhSachSanPham.add(sanPham);
+        }
+
+        System.out.println("\nCác sản phẩm của hãng Nokia:");
+        for (SanPham_bai2l6 sanPham : danhSachSanPham) {
+            if (sanPham.getHang().equalsIgnoreCase("Nokia")) {
+                sanPham.xuat();
+            }
+        }
+    }
 }
